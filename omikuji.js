d06ddf0db71b2c2ac9573322ce393ffe49b7ecdd
Kentaro Matsubara <kusa.sourcetree@airmail.cc>
2022-11-30 22:59:24 +0900

    [Add] add random gen

--- omikuji.js
+++ omikuji.js
@@ -3,3 +3,20 @@ let userresult;
 
 username = prompt("‚¨–¼‘O‚ğ‹³‚¦‚Ä‰º‚³‚¢B");
 document.getElementById("name").innerHTML = username;
+
+let rand = Math.floor( Math.random() * 5) ;
+if (rand == 0) {
+  userresult = "‘å‹g";
+}
+if (rand == 1) {
+  userresult = "’†‹g";
+}
+if (rand == 2) {
+  userresult = "¬‹g";
+}
+if (rand == 3) {
+  userresult = "‹g";
+}
+if (rand == 4) {
+  userresult = "‹¥";
+}
